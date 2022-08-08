d972f59e6340b2cf845292ecbb090a9b92e42179
Mohamed Abuzaid <Mohamedzabuzaid@gmail.com>
2022-08-08 01:16:10 -0700

    Updates to all files

--- generator.js
+++ generator.js
@@ -6,13 +6,30 @@ function random_number_index(){
 x = random_number_index();
 console.log(x)
 
-function tarot_card(){
-    card = ['The Fool', 'The Magician', 'The High Priestess', 'The Empress', 'The Emperor', 'The Hierophant', 'The Lovers', 'The Chariot', 'Strength', 'The Hermit', ' Wheel of Fortune', 'Justice', 'The Hanged Man', 'Death', 'Temperance' , ' The Devil', 'The Tower', 'The Star', 'The Moon', 'The Sun', 'Judgment', 'The World'];
-    reader_message = ['This is good!', 'This is bad.'];
-    what_to_do = ['Be careful.', 'Buy a lottery ticket.', 'Can not do anything'];
+tarot_card = {
+
+    card: ['The Fool', 'The Magician', 
+    'The High Priestess', 'The Empress', 
+    'The Emperor', 'The Hierophant', 
+    'The Lovers', 'The Chariot', 
+    'Strength', 'The Hermit', 
+    'Wheel of Fortune', 'Justice', 
+    'The Hanged Man', 'Death', 'Temperance',
+    'The Devil', 'The Tower', 'The Star', 
+    'The Moon', 'The Sun', 'Judgment', 
+    'The World'],
+
+    reader_message: ['This is good!', 'This is bad.'],
+
+    what_to_do: ['Be careful.', 'Buy a lottery ticket.',
+    'Can not do anything']
 }
 
 let user_future = [];
+for (let prop in tarot_card){
+
+
+}
 
 function output(array) {
 
