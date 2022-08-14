f8ea9fbdd9f21c1b70537ac11a4af951411896b8
Mohamed Abuzaid <Mohamedzabuzaid@gmail.com>
2022-08-13 18:18:27 -0700

    Project complete. Added tarot card images

--- generator.js
+++ generator.js
@@ -1,14 +1,12 @@
 function random_number_index(){
-    Number = Math.floor(Math.random() * 100);
+    Number = Math.floor(Math.random() * 21);
 
     return Number;
 }
-x = random_number_index();
-console.log(x)
 
-tarot_card = {
 
-    card: ['The Fool', 'The Magician', 
+
+    card = ['The Fool', 'The Magician', 
     'The High Priestess', 'The Empress', 
     'The Emperor', 'The Hierophant', 
     'The Lovers', 'The Chariot', 
@@ -17,23 +15,20 @@ tarot_card = {
     'The Hanged Man', 'Death', 'Temperance',
     'The Devil', 'The Tower', 'The Star', 
     'The Moon', 'The Sun', 'Judgment', 
-    'The World'],
+    'The World'];
+    imgarr = ['Images/0-The-Fool.jpg', 'Images/1-The-Magician.jpg', 'Images/2-The-High-Priestess.jpg', 'Images/3-The-Empress.jpg', 'Images/4-The-Emperor.jpg', 'Images/5-The-Hierophant.jpg', 'Images/6-The-Lovers.jpg', 'Images/7-The-Chariot.jpg', 'Images/8-Strength.jpg' , 'Images/9-The-Hermit.jpg', 'Images/10-The-Wheel-of-Fortune.jpg', 'Images/11-Justice.jpg', 'Images/12-The-Hanged-Man.jpg', 'Images/13-Death.jpg', 'Images/14-Temperance.jpg', 'Images/15-The-Devil.jpg', 'Images/16-The-Tower.jpg', 'Images/17-The-Star.jpg', 'Images/18-The-Moon.jpg', 'Images/19-The-Sun.jpg', 'Images/20-Judgment.jpg', 'Images/21-The-World.jpg']
 
-    reader_message: ['This is good!', 'This is bad.'],
-
-    what_to_do: ['Be careful.', 'Buy a lottery ticket.',
-    'Can not do anything']
-}
-
-function make_for(){
-let user_future = [];
-for (let prop in tarot_card){
-
-}
-}
+    
+        
 function output() {
 
-    let result = 'mohamed'
-    document.getElementById("stuff").innerHTML = result;
+    let result = random_number_index()
+    document.getElementById("stuff").innerHTML = card[result];
+
+    var img = document.createElement("img"); 
+    img.src = imgarr[result]; 
+    console.log(img.src)
+    var src = document.getElementById("tarot"); 
+    src.appendChild(img); 
     
 }
